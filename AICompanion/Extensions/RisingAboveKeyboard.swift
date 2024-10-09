1b38b77baf7997dff554d046c367d91aff20116c
Satin91 <cool19912015@gmail.com>
2024-10-09 16:07:04 +0300

    *Created view modifier for view when keyboard is appear **Created Messages view as differend view

---
+++ AICompanion/Extensions/RisingAboveKeyboard.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModifiers.swift
+//  AICompanion
+//
+//  Created by Артур Кулик on 09.10.2024.
+//
+
+import Foundation
---
+++ AICompanion/Screens/Chat/MessagesView.swift
@@ -0,0 +1,8 @@
+//
+//  MessagesView.swift
+//  AICompanion
+//
+//  Created by Артур Кулик on 09.10.2024.
+//
+
+import Foundation
